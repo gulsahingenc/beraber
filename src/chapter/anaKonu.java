417170fb879dbb70b53fc0fdf79fc7f10d9f4dec
Gulsah <gulsahingenc@gmail.com>
2022-12-09 14:42:34 +0000

    gulsah branch

--- src/chapter/anaKonu.java
+++ src/chapter/anaKonu.java
@@ -5,5 +5,7 @@ public class anaKonu {
     public static void main(String[] args) {
         System.out.println("main dosya");
         System.out.println("ikinci degisiklik");
+
+        System.out.println("gulsah branch");
     }
 }
